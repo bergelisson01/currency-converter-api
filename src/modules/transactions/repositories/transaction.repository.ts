5ae847799fd7472e68f8a57c95d12e8fd6ef6da3
Berg Élisson <bergelisson01@gmail.com>
2025-05-30 01:49:05 -0300

    fixing import

--- src/modules/transactions/repositories/transaction.repository.ts
+++ src/modules/transactions/repositories/transaction.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../shared/prisma/prisma.service';
-import { Transaction } from ' ../../../../generated/prisma';
+import { Transaction } from '@prisma/client'
 
 @Injectable()
 export class TransactionRepository {
